5ab228296ed59f3743598c19888c4874af3aa1dc
Charles Korn <me@charleskorn.com>
2022-06-02 19:58:19 +1000

    Simplify application startup.

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const PORT =  process.env.PORT;
 
 app.use(express.static('public'));
 
-console.log('Setting timeout...');
-setTimeout(() => {
-  console.log('About to listen...');
-  app.listen(PORT, () => {
-    console.log(`App now listening on port ${PORT}`);
-  })
-}, 10000)
+app.listen(PORT, () => {
+  console.log(`App now listening on port ${PORT}`);
+});
